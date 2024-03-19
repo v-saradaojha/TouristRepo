18d420e12e091ef3f755e89724d3a44b371cf937
Sarada Ojha <v-saradaojha@microsoft.com>
2024-03-19 13:26:27 +0530

    updated agent

--- TheTourist/client-app/src/app/api/agent.ts
+++ TheTourist/client-app/src/app/api/agent.ts
@@ -59,8 +59,8 @@ axios.interceptors.response.use(
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    post: <T>(url: string, body: NonNullable<T>) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: NonNullable<T>) => axios.put<T>(url, body).then(responseBody),
     delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
 
